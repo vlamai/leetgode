983364f74ebfcb932b42b6632af079a85ea3e064
Vla Mai <42vlamai@gmail.com>
2022-06-03 18:47:20 +0400

    solution 136. Single Number

--- 136_Single_Number/main.go
+++ 136_Single_Number/main.go
@@ -4,5 +4,9 @@ package _36_Single_Number
 //
 // You must implement a solution with a linear runtime complexity and use only constant extra space.
 func singleNumber(nums []int) int {
-	return 0
+	var result int
+	for _, num := range nums {
+		result ^= num
+	}
+	return result
 }
