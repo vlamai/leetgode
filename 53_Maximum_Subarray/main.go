396158f912a98666aea2dc70df77cf468788eead
Vla Mai <42vlamai@gmail.com>
2022-06-03 20:20:04 +0400

    solution 53. Maximum Subarray

--- 53_Maximum_Subarray/main.go
+++ 53_Maximum_Subarray/main.go
@@ -6,6 +6,18 @@ package _3_Maximum_Subarray
 // A subarray is a contiguous part of an array.
 
 // -2, 1, -3, 4, -1, 2, 1, -5, 4
+//            4, -1, 2, 1
 func maxSubArray(nums []int) int {
-	return -1
+	maxSum := nums[0]
+	cSum := 0
+	for _, num := range nums {
+		if cSum < 0 {
+			cSum = 0
+		}
+		cSum += num
+		if maxSum < cSum {
+			maxSum = cSum
+		}
+	}
+	return maxSum
 }
