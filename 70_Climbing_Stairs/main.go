602f4636e8116400072032695df9b0ff3854db8c
Vla Mai <42vlamai@gmail.com>
2022-06-03 19:11:17 +0400

    solution 70. Climbing Stairs

--- 70_Climbing_Stairs/main.go
+++ 70_Climbing_Stairs/main.go
@@ -5,5 +5,15 @@ package _0_Climbing_Stairs
 // Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
 func climbStairs(n int) int {
-	return 0
+	if n < 3 {
+		return n
+	}
+	f := 1
+	s := 2
+	for i := 2; i < n; i++ {
+		c := f + s
+		f = s
+		s = c
+	}
+	return s
 }
