927f48e400aafe3a14b67cbb312f3209d4050f5e
Vla Mai <42vlamai@gmail.com>
2022-06-03 19:19:13 +0400

    solution 121. Best Time to Buy and Sell Stock

--- 121_Best_Time_to_Buy_and_Sell_Stock/main.go
+++ 121_Best_Time_to_Buy_and_Sell_Stock/main.go
@@ -7,6 +7,18 @@ package _21_Best_Time_to_Buy_and_Sell_Stock
 //
 // Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
+// 7, 1, 5, 3, 6, 4
 func maxProfit(prices []int) int {
-	return -1
+	minPrice := prices[0]
+	var maxProfit = 0
+	for _, price := range prices {
+		if minPrice > price {
+			minPrice = price
+		}
+		curProfit := price - minPrice
+		if curProfit > maxProfit {
+			maxProfit = curProfit
+		}
+	}
+	return maxProfit
 }
