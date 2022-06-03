2681992863c0290fbf702023b2c0f15e2490ce75
Vla Mai <42vlamai@gmail.com>
2022-06-03 22:11:55 +0400

    bad solution 704. Binary Search

--- 704_Binary_Search/main.go
+++ 704_Binary_Search/main.go
@@ -5,5 +5,17 @@ package _04_Binary_Search
 //
 // You must write an algorithm with O(log n) runtime complexity.
 func search(nums []int, target int) int {
-	return -42
+	l := 0
+	r := len(nums) - 1
+	for l < r {
+		mid := (l + r) / 2
+		if nums[mid] == target {
+			return mid
+		} else if target > nums[mid] {
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+	}
+	return -1
 }
