29468ef703a88de62c5d5bf249f5d0968f201dbb
Vla Mai <42vlamai@gmail.com>
2022-06-03 19:24:28 +0400

    description 53. Maximum Subarray

--- 53_Maximum_Subarray/main.go
+++ 53_Maximum_Subarray/main.go
@@ -1,5 +1,9 @@
 package _3_Maximum_Subarray
 
+// Given an integer array nums, find the contiguous subarray (containing at least one number)
+// which has the largest sum and return its sum.
+//
+// A subarray is a contiguous part of an array.
 func maxSubArray(nums []int) int {
 	return -1
 }
