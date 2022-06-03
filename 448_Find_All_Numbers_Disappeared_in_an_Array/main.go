5ccf3df3c2f27fcde3ffd75a3cb5f254a27006e6
Vla Mai <42vlamai@gmail.com>
2022-06-03 18:42:35 +0400

    solution 448. Find All Numbers Disappeared in an Array

--- 448_Find_All_Numbers_Disappeared_in_an_Array/main.go
+++ 448_Find_All_Numbers_Disappeared_in_an_Array/main.go
@@ -5,5 +5,20 @@ package _48_Find_All_Numbers_Disappeared_in_an_Array
 // return an array of all the integers in the range [1, n] that do not appear in nums.
 //
 func findDisappearedNumbers(nums []int) []int {
-	return nil
+	var result []int
+	for _, num := range nums {
+		if num < 0 {
+			num *= -1
+		}
+		if nums[num-1] < 0 {
+			continue
+		}
+		nums[num-1] *= -1
+	}
+	for i, num := range nums {
+		if num > 0 {
+			result = append(result, i+1)
+		}
+	}
+	return result
 }
