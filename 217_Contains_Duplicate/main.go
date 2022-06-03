1eaa735aa2255612a6369b9a722de670bd687a22
Vla Mai <42vlamai@gmail.com>
2022-06-03 17:54:29 +0400

    solution 217. Contains Duplicate

--- 217_Contains_Duplicate/main.go
+++ 217_Contains_Duplicate/main.go
@@ -5,5 +5,12 @@ package _17_Contains_Duplicate
 // and return false if every element is distinct.
 //
 func containsDuplicate(nums []int) bool {
+	set := make(map[int]struct{})
+	for _, num := range nums {
+		if _, ok := set[num]; ok {
+			return true
+		}
+		set[num] = struct{}{}
+	}
 	return false
 }
