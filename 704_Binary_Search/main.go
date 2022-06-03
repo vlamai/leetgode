1158d10f81a15237049ea2050d8c0b953fd806c3
Vla Mai <42vlamai@gmail.com>
2022-06-03 22:14:15 +0400

    solution 704. Binary Search

--- 704_Binary_Search/main.go
+++ 704_Binary_Search/main.go
@@ -7,7 +7,7 @@ package _04_Binary_Search
 func search(nums []int, target int) int {
 	l := 0
 	r := len(nums) - 1
-	for l < r {
+	for l <= r {
 		mid := (l + r) / 2
 		if nums[mid] == target {
 			return mid
