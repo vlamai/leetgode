ccaaa80ddab34b5b0358f52aabe511cae02b1f59
Vla Mai <42vlamai@gmail.com>
2022-06-03 21:12:10 +0400

    solution 338. Counting Bits

--- 338_Counting_Bits/main.go
+++ 338_Counting_Bits/main.go
@@ -4,5 +4,9 @@ package _38_Counting_Bits
 // such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
 
 func countBits(n int) []int {
-	return nil
+	result := []int{0}
+	for i := 1; i <= n; i++ {
+		result = append(result, result[i>>1]+i%2)
+	}
+	return result
 }
