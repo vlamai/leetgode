0580820c2251d111e6151f75bce731206700bb9b
Vla Mai <42vlamai@gmail.com>
2022-06-04 15:28:31 +0400

    solution 33. Search in Rotated Sorted Array

--- 33_Search_in_Rotated_Sorted_Array/main.go
+++ 33_Search_in_Rotated_Sorted_Array/main.go
@@ -12,5 +12,27 @@ package _3_Search_in_Rotated_Sorted_Array
 // You must write an algorithm with O(log n) runtime complexity.
 
 func search(nums []int, target int) int {
-	return 0
+	l := 0
+	r := len(nums) - 1
+	for l <= r {
+		mid := (l + r) / 2
+		if target == nums[mid] {
+			return mid
+		}
+		if nums[l] <= nums[mid] {
+			if target > nums[mid] || target < nums[l] {
+				l = mid + 1
+			} else {
+				r = mid - 1
+			}
+		} else {
+			if target < nums[mid] || target > nums[r] {
+				r = mid - 1
+			} else {
+				l = mid + 1
+			}
+		}
+	}
+
+	return -1
 }
