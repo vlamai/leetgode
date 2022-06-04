27cdc7c82ab2734edfb28ba4649c2bf29a46dded
Vla Mai <42vlamai@gmail.com>
2022-06-04 12:35:30 +0400

    test case 152. Maximum Product Subarray

--- 152_Maximum_Product_Subarray/main_test.go
+++ 152_Maximum_Product_Subarray/main_test.go
@@ -42,6 +42,13 @@ func Test_maxProduct(t *testing.T) {
 			},
 			want: -2,
 		},
+		{
+			name: "Test 5",
+			args: args{
+				nums: []int{-3, -1, -1},
+			},
+			want: 3,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
