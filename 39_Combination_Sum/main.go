3d308c877595963237bc25354fb3fb6f4aab57c9
Vla Mai <42vlamai@gmail.com>
2022-06-04 11:04:52 +0400

    no solution 39. Combination Sum

--- 39_Combination_Sum/main.go
+++ 39_Combination_Sum/main.go
@@ -1,5 +1,21 @@
 package _9_Combination_Sum
 
 func combinationSum(candidates []int, target int) [][]int {
-	return nil
+	var res [][]int
+	r := dfs(0, 0, target, []int{}, candidates)
+	res = append(res, r)
+	return res
+}
+
+func dfs(i, total, target int, cur, candidates []int) []int {
+	if total == target {
+		return cur
+	}
+	if i >= len(candidates) || total > target {
+		return nil
+	}
+	cur = append(cur, candidates[i])
+	dfs(i, total+candidates[i], target, cur, candidates)
+	dfs(i+1, total+candidates[i], target, cur[:len(cur)-1], candidates)
+	return cur
 }
