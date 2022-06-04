a940f40956e26fe835680e8ffcd8f2f8fcc88951
Vla Mai <42vlamai@gmail.com>
2022-06-04 17:38:33 +0400

    solution 11. Container With Most Water

--- 11_Container_With_Most_Water/main.go
+++ 11_Container_With_Most_Water/main.go
@@ -11,6 +11,39 @@ package _1_Container_With_Most_Water
 //
 // Notice that you may not slant the container.
 func maxArea(height []int) int {
+	l := 0
+	r := len(height) - 1
+	maxA := 0
+	for l <= r {
+		leftHeight := height[l]
+		rightHeight := height[r]
+		area := (r - l) * min(leftHeight, rightHeight)
+		maxA = max(area, maxA)
+		if leftHeight >= rightHeight {
+			r--
+		} else {
+			l++
+		}
+	}
+	return maxA
+}
 
-	return 0
+func min(nums ...int) int {
+	m := nums[0]
+	for _, num := range nums {
+		if m > num {
+			m = num
+		}
+	}
+	return m
+}
+
+func max(nums ...int) int {
+	m := nums[0]
+	for _, num := range nums {
+		if num > m {
+			m = num
+		}
+	}
+	return m
 }
