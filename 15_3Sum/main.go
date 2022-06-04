931f61d1534bd27b4c84f066a21373eb55972b7c
Vla Mai <42vlamai@gmail.com>
2022-06-04 15:49:07 +0400

    solution 15. 3Sum

--- 15_3Sum/main.go
+++ 15_3Sum/main.go
@@ -1,9 +1,41 @@
 package _5_3Sum
 
+import (
+	"sort"
+)
+
 // Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]
 // such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
 //
 // Notice that the solution set must not contain duplicate triplets.
 func threeSum(nums []int) [][]int {
-	return nil
+	res := [][]int{}
+	if len(nums) < 3 {
+		return res
+	}
+	sort.Ints(nums)
+	for i, num := range nums {
+		if i > 0 && num == nums[i-1] {
+			continue
+		}
+
+		l := i + 1
+		r := len(nums) - 1
+		for l < r {
+			ts := num + nums[l] + nums[r]
+			if ts > 0 {
+				r--
+			} else if ts < 0 {
+				l++
+			} else {
+				res = append(res, []int{num, nums[l], nums[r]})
+				l++
+				for nums[l] == nums[l-1] && l < r {
+					l++
+				}
+			}
+		}
+	}
+
+	return res
 }
