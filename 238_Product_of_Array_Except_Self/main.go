b428f1c9fa34a7cd6cd61838d6dcaf915baba8a3
Vla Mai <42vlamai@gmail.com>
2022-06-04 12:13:05 +0400

    solution 238. Product of Array Except Self

--- 238_Product_of_Array_Except_Self/main.go
+++ 238_Product_of_Array_Except_Self/main.go
@@ -8,5 +8,34 @@ package _38_Product_of_Array_Except_Self
 // You must write an algorithm that runs in O(n) time and without using the division operation.
 
 func productExceptSelf(nums []int) []int {
-	return nums
+	numsLen := len(nums)
+	lastIndex := numsLen - 1
+	l := make([]int, numsLen)
+	r := make([]int, numsLen)
+	for i := 0; i < numsLen; i++ {
+		l[i] = nums[i]
+		if i > 0 {
+			l[i] = nums[i] * l[i-1]
+		}
+	}
+
+	for i := lastIndex; i >= 0; i-- {
+		r[i] = nums[i]
+		if i != lastIndex {
+			r[i] = nums[i] * r[i+1]
+		}
+	}
+	var res = make([]int, numsLen)
+	for i := 0; i < numsLen; i++ {
+		lv := 1
+		rv := 1
+		if i != 0 {
+			lv = l[i-1]
+		}
+		if i != lastIndex {
+			rv = r[i+1]
+		}
+		res[i] = lv * rv
+	}
+	return res
 }
