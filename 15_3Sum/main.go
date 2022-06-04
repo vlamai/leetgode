ccca814bbcfd5397933371330a7dd4d0ab9d27f6
Vla Mai <42vlamai@gmail.com>
2022-06-04 15:33:34 +0400

    description 15. 3Sum

--- 15_3Sum/main.go
+++ 15_3Sum/main.go
@@ -1,5 +1,9 @@
 package _5_3Sum
 
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]
+// such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+//
+// Notice that the solution set must not contain duplicate triplets.
 func threeSum(nums []int) [][]int {
 	return nil
 }
