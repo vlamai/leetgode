413fb250a2f028c79031384a2eb8b95290a0f15b
Vla Mai <42vlamai@gmail.com>
2022-06-04 13:06:15 +0400

    solution ? 152. Maximum Product Subarray

--- 152_Maximum_Product_Subarray/main.go
+++ 152_Maximum_Product_Subarray/main.go
@@ -9,15 +9,36 @@ package _52_Maximum_Product_Subarray
 
 func maxProduct(nums []int) int {
 	maxProd := nums[0]
-	curProd := nums[0]
-	for _, num := range nums[1:] {
-		if curProd <= 0 {
-			curProd = 1
+	curMin, curMax := 1, 1
+	for _, num := range nums {
+		if num == 0 {
+			curMin, curMax = 1, 1
 		}
-		curProd *= num
-		if maxProd < curProd {
-			maxProd = curProd
-		}
+		tmpMax := num * curMax
+		tmpMin := num * curMin
+		curMax = max(tmpMax, tmpMin, num)
+		curMin = min(tmpMax, tmpMin, num)
+		maxProd = max(curMax, maxProd)
 	}
 	return maxProd
+}
+
+func min(i ...int) int {
+	m := i[0]
+	for _, n := range i {
+		if m > n {
+			m = n
+		}
+	}
+	return m
+}
+
+func max(i ...int) int {
+	m := i[0]
+	for _, n := range i[1:] {
+		if n > m {
+			m = n
+		}
+	}
+	return m
 }
