9d58d2990b1439ad19b949b503e27b8ddd8bd20e
Vla Mai <42vlamai@gmail.com>
2022-06-05 12:33:40 +0400

    test 39. Combination sum

--- 39_Combination_Sum/main_test.go
+++ 39_Combination_Sum/main_test.go
@@ -1,51 +1,39 @@
 package _9_Combination_Sum
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
+type args struct {
+	candidates []int
+	target     int
+}
+
+type testCase struct {
+	name string
+	args args
+	want [][]int
+}
+
+func tc(candidates []int, target int, want [][]int) testCase {
+	name := fmt.Sprintf("candidates: %v, target: %d", candidates, target)
+	return testCase{
+		name: name,
+		args: args{
+			candidates: candidates,
+			target:     target,
+		},
+		want: want,
+	}
+}
 func Test_combinationSum(t *testing.T) {
-	type args struct {
-		candidates []int
-		target     int
-	}
-	tests := []struct {
-		name string
-		args args
-		want [][]int
-	}{
-		{
-			name: "Test 1",
-			args: args{
-				candidates: []int{2, 3, 6, 7},
-				target:     7,
-			},
-			want: [][]int{
-				{2, 2, 3},
-			},
-		},
-		{
-			name: "Test 2",
-			args: args{
-				candidates: []int{2, 3, 5},
-				target:     8,
-			},
-			want: [][]int{
-				{2, 2, 2, 2},
-				{2, 3, 3},
-				{3, 5},
-			},
-		},
-		{
-			name: "Test 3",
-			args: args{
-				candidates: []int{2},
-				target:     1,
-			},
-			want: [][]int{{}},
-		},
-	}
+	var tests []testCase
+
+	tests = append(tests, tc([]int{2, 3, 6, 7}, 7, [][]int{{2, 2, 3}, {7}}))
+	tests = append(tests, tc([]int{2, 3, 5}, 8, [][]int{{2, 2, 2, 2}, {2, 3, 3}, {3, 5}}))
+	tests = append(tests, tc([]int{2}, 1, [][]int{{}}))
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			assert.Equal(t, tt.want, combinationSum(tt.args.candidates, tt.args.target))
