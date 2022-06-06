44c8e69ee58b66d9316cea1eceb297dc485bdd9d
Vla Mai <42vlamai@gmail.com>
2022-06-06 22:48:53 +0400

    solution 2. Add Two Numbers

--- 2_Add_Two_Numbers/main.go
+++ 2_Add_Two_Numbers/main.go
@@ -5,5 +5,38 @@ import (
 )
 
 func addTwoNumbers(l1 *l.ListNode, l2 *l.ListNode) *l.ListNode {
-	return nil
+	extra := 0
+	res := &l.ListNode{}
+	head := res
+	for {
+		sum := extra
+		if l1 != nil {
+			sum += l1.Val
+		}
+		if l2 != nil {
+			sum += l2.Val
+		}
+
+		extra = sum / 10
+
+		res.Val = sum % 10
+
+		if l1 != nil {
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			l2 = l2.Next
+		}
+
+		if l1 == nil && l2 == nil {
+			if extra > 0 {
+				res.Next = &l.ListNode{Val: extra}
+			}
+			break
+		}
+		res.Next = &l.ListNode{}
+		res = res.Next
+
+	}
+	return head
 }
