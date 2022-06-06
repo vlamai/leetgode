a53f3c80f7cfbc7b720b4e3bfde5b223a5c56b32
Vla Mai <42vlamai@gmail.com>
2022-06-06 22:54:53 +0400

    solution 74. Search a 2D Matrix

--- 74_Search_a_2D_Matrix/main.go
+++ 74_Search_a_2D_Matrix/main.go
@@ -1,5 +1,14 @@
 package _4_Search_a_2D_Matrix
 
 func searchMatrix(matrix [][]int, target int) bool {
+	for _, row := range matrix {
+		if row[len(row)-1] >= target {
+			for _, cell := range row {
+				if cell == target {
+					return true
+				}
+			}
+		}
+	}
 	return false
 }
