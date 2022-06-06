d57ad4bde166e92459f2b3f15c9bd1fb97388e64
Vla Mai <42vlamai@gmail.com>
2022-06-06 23:02:33 +0400

    test 844. Backspace String Compare

---
+++ 844_Backspace_String_Compare/main_test.go
@@ -0,0 +1,48 @@
+package _44_Backspace_String_Compare
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_backspaceCompare(t *testing.T) {
+	type args struct {
+		s string
+		t string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Test 1",
+			args: args{
+				s: "ab#c",
+				t: "ad#c",
+			},
+			want: true,
+		},
+		{
+			name: "Test 2",
+			args: args{
+				s: "ab##",
+				t: "c#d#",
+			},
+			want: false,
+		},
+		{
+			name: "Test 3",
+			args: args{
+				s: "a#c",
+				t: "b",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, backspaceCompare(tt.args.s, tt.args.t))
+		})
+	}
+}
