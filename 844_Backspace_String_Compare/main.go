bb0b93e2cf4a199724598593584e8b3ebb8ec76a
Vla Mai <42vlamai@gmail.com>
2022-06-06 23:03:42 +0400

    solution 844. Backspace String Compare

--- 844_Backspace_String_Compare/main.go
+++ 844_Backspace_String_Compare/main.go
@@ -1,5 +1,22 @@
 package _44_Backspace_String_Compare
 
 func backspaceCompare(s string, t string) bool {
-	return false
+	return getString(s) == getString(t)
+}
+
+func getString(s string) (r string) {
+	var d int
+	for i := len(s) - 1; i >= 0; i-- {
+		l := s[i]
+		if l == '#' {
+			d++
+			continue
+		}
+		if d > 0 {
+			d--
+			continue
+		}
+		r += string(l)
+	}
+	return
 }
