eb11db54e83510b6c20607371e05adbfaba08634
Vla Mai <42vlamai@gmail.com>
2022-06-09 14:00:50 +0400

    solution 242. Valid Anagram

--- 242_Valid_Anagram/main.go
+++ 242_Valid_Anagram/main.go
@@ -1,5 +1,30 @@
 package _42_Valid_Anagram
 
 func isAnagram(s string, t string) bool {
-	return false
+	return isAnagramMap(s, t)
+}
+
+func isAnagramMap(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+	var set = make(map[int32]int)
+	for _, c := range s {
+		if _, ok := set[c]; ok {
+			set[c]++
+		} else {
+			set[c] = 1
+		}
+	}
+	for _, c := range t {
+		if _, ok := set[c]; ok {
+			set[c]--
+			if set[c] < 0 {
+				return false
+			}
+		} else {
+			return false
+		}
+	}
+	return true
 }
