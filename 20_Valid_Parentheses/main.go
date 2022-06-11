a97d63ec99d943ea63ddbe3fb914ad45499e4ddd
Vla Mai <42vlamai@gmail.com>
2022-06-11 11:50:05 +0400

    solution 20. Valid Parentheses

--- 20_Valid_Parentheses/main.go
+++ 20_Valid_Parentheses/main.go
@@ -1,5 +1,21 @@
 package _0_Valid_Parentheses
 
 func isValid(s string) bool {
-	return false
+	stack := make([]rune, 0)
+	for _, c := range s {
+		switch c {
+		case '(':
+			stack = append(stack, ')')
+		case '[':
+			stack = append(stack, ']')
+		case '{':
+			stack = append(stack, '}')
+		default:
+			if len(stack) == 0 || stack[len(stack)-1] != c {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+	return len(stack) == 0
 }
